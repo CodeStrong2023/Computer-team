a678a4df3accf6823fba9b83238628caa98ac2fe
Lucas <lucassarmiento97@gmail.com>
2023-11-02 16:46:06 -0300

    Añadimos ejercicios correspondientes a clase 5 de Python

---
+++ LucasSarmiento/Python/Clase5/mainClase5.py
@@ -0,0 +1,11 @@
+#Una función siempre necesita ser llamdada para que se ejecute el codigo que está dentro de ese bloque
+#Comenzamos con Funciones
+#Definimos una función
+# mi_funcion() #no se puede llamar antes de definir una función
+
+def mi_funcion(): #Para identificar la función utilizamos paréntesis
+    print("Saludos a todos los alumnos de la Tecnicatura")
+
+mi_funcion() #Estamos llamando a la funcion 
+mi_funcion()
+mi_funcion()
